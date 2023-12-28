42f53682b397a4dc97969982c28c826dce2bd07b
hailemedn <hailemedhin.k@gmail.com>
2023-12-28 23:33:32 +0300

    prepared the semantic html and inserted content, and wrote the meta tags and ogp for better seo and rankings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HomePage</h1>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from '../images/Logo .svg'
+const Header = () => {
+    return (
+        <header>
+            <img src={logo} alt='logo'></img>
+        </header>
+
+        )
+}
+
+export default Header;
